231d2acc8537ac9da6c7a7ce817276a8c1d63844
wyt <wangyuting@dfgroup.pro>
2021-12-22 18:20:43 +0800

    add-topBar

---
+++ packages/theme/components/TopBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <SfTopBar class="topbar">
+    <template #center>
+      <!-- <p>{{ $t('Download') }}</p> -->
+      <SfButton class="topbar__button sf-button--text">{{
+        $t(' Welcome to our store')
+      }}</SfButton>
+    </template>
+  </SfTopBar>
+</template>
+
+<script>
+import { SfButton, SfTopBar } from '@storefront-ui/vue';
+import LocaleSelector from './LocaleSelector';
+
+export default {
+  components: {
+    SfTopBar,
+    SfButton,
+    LocaleSelector
+  }
+};
+</script>
+<style lang="scss" scoped>
+.topbar {
+  position: relative;
+  z-index: 2;
+  &__button {
+    margin: 0 0 0 var(--spacer-xs);
+  }
+}
+</style>
